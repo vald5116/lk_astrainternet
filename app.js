682fe3e83cbb4115e9edc7d16465a88538db1137
vald5116 <vald5116@gmail.com>
2019-11-19 03:55:27 +0400

    Добавил коментарии

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ var
 
 
 
-needle.get(`${config.url}/auth/login`,
+needle.get(`${config.url}/auth/login`, //Заходим на страницу авториза
 	  function(err, resp, body) {
-	  	var $ = cheerio.load(body);
-  		config.account['_token'] = $("input[name=_token]").val();
-		  needle.post(`${config.url}/auth/login`, config.account,
+	  	var $ = cheerio.load(body);		//Получаем страницу
+  		config.account['_token'] = $("input[name=_token]").val(); //Получаем возможно изменяемый токен на странице
+		  needle.post(`${config.url}/auth/login`, config.account, //Авторизуем пользователя 
 			  function(err, resp, body) {
-			  	needle.get(`${config.url}`, {cookies:resp.cookies},
+			  	needle.get(`${config.url}`, {cookies:resp.cookies}, //Переходим на главную
 				  function(err, resp, body) {
-				  		var $ = cheerio.load(body);
-				  		var sum = Number($(".money").text().replace(/\D+/g,"").slice(0,-2)+'.'+$(".money").text().replace(/\D+/g,"").substr($(".money").text().replace(/\D+/g,"").length - 2))
+				  		$ = cheerio.load(body);	//Получаем страницу
+				  		var sum = Number($(".money").text().replace(/\D+/g,"").slice(0,-2)+'.'+$(".money").text().replace(/\D+/g,"").substr($(".money").text().replace(/\D+/g,"").length - 2)) //Находим наш баланс
 						console.log(sum)
 				});
 			}); 
